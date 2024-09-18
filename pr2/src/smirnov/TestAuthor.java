9c61ec52a875a654f24a09e425d7c978174bbe4e
SpartakSS20 <spartsmirnik@gmail.com>
2024-09-18 22:59:26 +0300

    Add files via upload

---
+++ pr2/src/smirnov/TestAuthor.java
@@ -0,0 +1,8 @@
+package smirnov;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+    Author a1 = new Author("Kirill", "Kirillkong@mail.ru", 'M');
+    System.out.println(a1);
+    }
+}
