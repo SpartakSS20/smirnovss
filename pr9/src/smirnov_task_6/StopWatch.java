a14278a9dac9513724fd59b6baa3d1adffc66cf7
SpartakSS20 <spartsmirnik@gmail.com>
2024-11-06 23:31:51 +0300

    Add files via upload

---
+++ pr9/src/smirnov_task_6/StopWatch.java
@@ -0,0 +1,43 @@
+package smirnov_task_6;
+
+public class StopWatch {
+    private long startTime;
+    private long endTime;
+
+    public StopWatch() {
+        this.startTime = System.currentTimeMillis();
+        this.endTime = 0;
+    }
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public long getEndTime() {
+        return endTime;
+    }
+
+    public void start() {
+        this.startTime = System.currentTimeMillis();
+        this.endTime = 0; // Сбрасываем endTime
+    }
+
+    public void stop() {
+        this.endTime = System.currentTimeMillis();
+    }
+
+    public long getElapsedTime() {
+        if (endTime == 0) {
+            return System.currentTimeMillis() - startTime;
+        } else {
+            return endTime - startTime;
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        StopWatch stopwatch = new StopWatch();
+        Thread.sleep(2000);
+        stopwatch.stop();
+        System.out.println("Прошедшее время: " + stopwatch.getElapsedTime() + " ms");
+    }
+}
